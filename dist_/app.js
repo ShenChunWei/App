465fd5581d99225046f374c042af0e0e973f1c98
ShenChunWei <chunwei900824@gmail.com>
2022-05-07 15:28:32 +0800

    commit

--- dist_/app.js
+++ dist_/app.js
@@ -1,6 +1,7 @@
 var div = document.getElementById("div-pop");
 var preloader = document.getElementById("preloader");
 var close = document.getElementById("close-button");
+var main_scan = document.getElementById("main-scan");
 var scan_icon = document.getElementById("scan-icon");
 var success = document.getElementById("success");
 var fail = document.getElementById("fail");
@@ -8,6 +9,7 @@ var pay = document.getElementById("pay");
 var rescan = document.getElementById("rescan");
 var scan = document.getElementById("scan");
 var stop_scan = document.getElementById("stop-scan");
+
 
 setTimeout(function(){
   preloader.style.display = "none";
@@ -25,15 +27,65 @@ close.onclick = function div_close() {
   div.style.display = "none";
 };
 
-rescan.onclick = function fail_close() {
-  fail.style.display = "none";
+main_scan.onclick = function main_scan() {
   Html5Qrcode.getCameras()
     .then((devices) => {
       if (devices && devices.length) {
-        scan.style.display = "block";
-        div.style.display = "none";
-        stop_scan.style.display = "block";
+        setTimeout(function(){
+          scan.style.display = "block";
+          stop_scan.style.display = "block";
+        },2500);
+        var cameraId = devices[0].id;
+        const html5QrCode = new Html5Qrcode("reader");
+        const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+          html5QrCode.stop();
+          stop_scan.style.display = "none";
+          scan.style.display = 'none';
+          if (decodedText == "https://success.com.tw") {
+            success.style.display = "block";
+          } else if (decodedText == "https://fail.com.tw") {
+            fail.style.display = "block";
+          }
+        };
 
+        const config = {
+          fps: 25,
+          aspectRatio: 0.461,
+          qrbox: {
+            width: 250,
+            height: 250
+          }
+        };
+        html5QrCode.start(
+          {
+            facingMode: "environment"
+          },
+          config,
+          qrCodeSuccessCallback
+        );
+
+        stop_scan.onclick = function close() {
+          html5QrCode.stop();
+          stop_scan.style.display = "none";
+          scan.style.display = "none";
+        };
+      }
+    })
+    .catch((err) => {
+      // handle err
+      console.log('error')
+    });
+};
+
+scan_icon.onclick = function qrcode() {
+  Html5Qrcode.getCameras()
+    .then((devices) => {
+      if (devices && devices.length) {
+        div.style.display = "none";
+        setTimeout(function(){
+          scan.style.display = "block";
+          stop_scan.style.display = "block";
+        },2500);
         var cameraId = devices[0].id;
         const html5QrCode = new Html5Qrcode("reader");
         const qrCodeSuccessCallback = (decodedText, decodedResult) => {
@@ -78,14 +130,16 @@ rescan.onclick = function fail_close() {
     });
 };
 
-scan_icon.onclick = function qrcode() {
+rescan.onclick = function rescan() {
+  fail.style.display = "none";
   Html5Qrcode.getCameras()
     .then((devices) => {
       if (devices && devices.length) {
-        scan.style.display = "block";
         div.style.display = "none";
-        stop_scan.style.display = "block";
-
+        setTimeout(function(){
+          scan.style.display = "block";
+          stop_scan.style.display = "block";
+        },2500);
         var cameraId = devices[0].id;
         const html5QrCode = new Html5Qrcode("reader");
         const qrCodeSuccessCallback = (decodedText, decodedResult) => {
@@ -137,3 +191,4 @@ function ShowTime() {
   var s = NowDate.getSeconds();
   document.getElementById("showbox").innerHTML = h + "時" + m + "分" + s + "秒";
 }
+
